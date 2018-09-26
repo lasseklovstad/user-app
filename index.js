f206f07ae255aa55ab38a8db270d1e9529497b95
Lasse.Klovstad <lasse.klovstad@gmail.com>
2018-09-26 11:49:30 +0200

    yo

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-var http = require('http');
-http.createServer(function(request,response){
-	response.write("Hello, world"+ new Date());
-	response.end();
-}).listen(process.env.PORT || 5000)
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8081;
+
+
+app.get('/api/user', function(req,res){
+	var user_id = req.param('id');
+  	var token = req.param('token');
+  	var geo = req.param('penis');
+	res.send(user_id + ' ' + token + ' ' + geo);
+});
+
+app.get('/login', function(req,res){
+	res.sendFile(__dirname+"/index.html");
+});
+
+app.listen(port);
+console.log('Server started! At http://localhost:' + port);
